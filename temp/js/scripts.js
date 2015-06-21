88b26e1dc208c38739b2a10845fea90adf2f37e7
luizsotero <fernandosotero@outlook.com>
2015-06-21 17:13:57 -0300

    Adding controls images

--- temp/js/scripts.js
+++ temp/js/scripts.js
@@ -14,3 +14,40 @@ function onMetronomeTick(){
 	$('#square'+position).addClass("red");
 	position++;
 }
+
+function selectControl(control){
+	$('.control_selected').removeClass("control_selected");
+	control.addClass("control_selected");
+}
+
+function selectSquare(square){
+	var control = $('.control_selected');
+	$('.control_selected').removeClass("control_selected");
+	if(control.attr('id') == 'start'){
+		$('.start').removeClass("start");
+	}
+	square.removeClass();
+	square.addClass('squareCell');
+	square.addClass(control.attr('id'));
+	square.attr('control', control.attr('id'));
+}
+
+$(document).on({
+	click: function(event){
+		if(event.handled !== true){
+			event.handled = true;
+			selectControl($(this));
+		}
+		return false;
+	}
+}, ".block");
+
+$(document).on({
+	click: function(event){
+		if(event.handled !== true){
+			event.handled = true;
+			selectSquare($(this));
+		}
+		return false;
+	}
+}, ".squareCell");
