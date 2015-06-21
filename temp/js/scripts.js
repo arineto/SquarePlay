04df55343076d57d4a3d055ac74d43ac0c62915d
Luiz Fernando da Silva Sotero <fernandosotero@outlook.com>
2015-06-21 16:11:37 -0300

    Toggle class on metronome click

--- temp/js/scripts.js
+++ temp/js/scripts.js
@@ -1,6 +1,16 @@
 
 $( document ).ready(function() {
     for (var i = 0; i <280; i++) {
-    	$('#board').append("<div id="+i+" class='squareCell'></div>");
+    	$('#board').append("<div id=square"+i+" class='squareCell'></div>");
 	}
 });
+
+var position = 0;
+
+function onMetronomeTick(){
+	if(position >0){
+		$('#square'+(position-1)).removeClass("red");
+	}
+	$('#square'+position).addClass("red");
+	position++;
+}
