0b7a45c63fdf0c20ae778fa015b01c6449a5a81c
Arimatea Neto <arineto30@gmail.com>
2015-08-01 10:04:10 -0300

    Alteracoes para o deploy

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.conf import settings
 
 # Create your views here.
+
+def home(request):
+
+	c = RequestContext(request)
+
+	c['static'] = settings.STATIC_URL
+
+	return render_to_response('board.html', c)
